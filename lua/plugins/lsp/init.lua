338c5192285dca2142e831d7cccffc5171745c1e
cal <caldotio@protonmail.com>
2022-07-07 19:58:50 +0200

    fix: :bug: fixed type + formatting

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,6 +1,6 @@
 local installer_ready, installer = pcall(require, 'nvim-lsp-installer')
 local config_ready, lspconfig = pcall(require, 'lspconfig')
-if (not installer_available) and (not config_available) then return end
+if (not installer_ready) and (not config_ready) then return end
 local keymap = require('plugins.lsp.keymap')
 
 require('plugins.lsp.diagnostics')
@@ -8,9 +8,9 @@ require('plugins.lsp.diagnostics')
 installer.setup {
   ui = {
     icons = {
-        server_installed = "✓",
-        server_pending = "➜",
-        server_uninstalled = "✗"
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
     }
   }
 }
@@ -28,25 +28,25 @@ local flags = {
 }
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
+  border = "rounded",
 })
 
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = "rounded",
 })
 
 lspconfig.util.default_config = vim.tbl_extend(
-    'force',
-    lspconfig.util.default_config,
-    {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = flags,
-    }
+  'force',
+  lspconfig.util.default_config,
+  {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = flags,
+  }
 )
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-for _, server in ipairs(lsp_installer.get_installed_servers()) do
+for _, server in ipairs(installer.get_installed_servers()) do
   local opts = require('plugins.lsp.serve.' .. server.name)
   lspconfig[server.name].setup(opts)
 end
